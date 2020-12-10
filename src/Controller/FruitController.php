2e1eea6d82964feafc213d2f6a0c7dd762680c60
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-12-10 19:15:34 -0300

    Nome da rota no plural.

--- src/Controller/FruitController.php
+++ src/Controller/FruitController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/fruit", name="fruit_")
+ * @Route("/fruits", name="fruit_")
  */
 class FruitController extends AbstractController
 {
